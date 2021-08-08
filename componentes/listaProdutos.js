a70c779eb3113ba2b499a8295aa9e676db1c0453
Estevam de Freitas Junior <estevamdefreitas37@gmail.com>
2021-08-08 20:06:58 -0300

    Adicionados novos produtos

--- componentes/listaProdutos.js
+++ componentes/listaProdutos.js
@@ -1,6 +1,9 @@
 const listaProdutos = `[{"img":"assets/imagens/alface.png","nome":"Alface", "preco":2.99},
 {"img":"assets/imagens/rucula.png","nome":"Rucula", "preco":1.79},
 {"img":"assets/imagens/espinafre.png","nome":"Espinafre", "preco":3.49},
-{"img":"assets/imagens/cenoura.png","nome":"Cenoura", "preco":2.35}]`
-
+{"img":"assets/imagens/cenoura.png","nome":"Cenoura", "preco":2.35},
+{"img":"assets/imagens/aipim.png","nome":"Aipim", "preco":1.25},
+{"img":"assets/imagens/repolho.png","nome":"Repolho", "preco":3.15},
+{"img":"assets/imagens/salsa.png","nome":"Salsa", "preco":0.75},
+{"img":"assets/imagens/batata.png","nome":"Batata", "preco":1.65}]`
 export default listaProdutos
