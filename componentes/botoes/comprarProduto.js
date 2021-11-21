9f11c673bdd006da6521a4533166edaacb8441df
Hugo Ferreira <hugoogle@terra.com.br>
2021-11-21 19:48:18 -0300

    Refatorado botao  comprar produto

--- componentes/botoes/comprarProduto.js
+++ componentes/botoes/comprarProduto.js
@@ -1,3 +1,5 @@
+import listaDeCompra from "../../js/listaDeCompra.js";
+
 const BotaoComprar = (codigo) => {
   const botaoComprar = document.createElement("button");
   botaoComprar.setAttribute("data-produto-comprar", codigo);
@@ -15,50 +17,22 @@ const adicionarProduto = (evento) => {
   const produtoPreco = produto.querySelector("[data-produto-preco]");
   const produtoCodigo = produto.querySelector("[data-produto-comprar]");
 
-  if (botaoComprar.type === "submit") {
-    let valorTotalProduto = 0;
-    const imagem = produtoImagem.src;
-    const descricao = produtoDescricao.textContent;
-    const quantidade = produtoQuantidade.value;
-    const preco = produtoPreco.textContent.substring(3);
-    const codigo = produtoCodigo.getAttribute("data-produto-comprar");
+  let valorTotalProduto = 0;
+  const imagem = produtoImagem.src;
+  const descricao = produtoDescricao.textContent;
+  const quantidade = produtoQuantidade.value;
+  const preco = produtoPreco.textContent.substring(3);
+  const codigo = produtoCodigo.getAttribute("data-produto-comprar");
 
-    valorTotalProduto = parseFloat(preco) * parseInt(quantidade);
+  valorTotalProduto = parseFloat(preco) * parseInt(quantidade);
 
-    const isCodigo = listaDeCompras.some(
-      (carrinho) => carrinho.codigo === codigo
-    );
-
-    if (isCodigo) {
-      listaDeCompras.forEach((carrinho) => {
-        if (carrinho.codigo === codigo) {
-          carrinho.preco += valorTotalProduto;
-
-          carrinho.quantidade =
-            parseInt(carrinho.quantidade) + parseInt(quantidade);
-        }
-      });
-    } else {
-      listaDeCompras.push({
-        codigo: codigo,
-        imagem: imagem,
-        descricao: descricao,
-        quantidade: quantidade,
-        preco: valorTotalProduto,
-      });
-    }
-    carregarProdutosNoCarrinho();
-  }
-};
-
-const carregarProdutosNoCarrinho = () => {
-  limparCarrinho();
-  const carrinhoVazio = document.querySelector("[data-carrinho-vazio]");
-  carrinhoVazio.textContent = "";
-  listaDeCompras.forEach(calcularListaDeCompra);
-  carrinhoValorTotal.textContent = `R$ ${valorTotal.toFixed(2)}`;
-  carrinhoQuantidadeTotal.textContent = `${quantidadeTotal} und.`;
-  carrinhoQuantidadeitens.textContent = `${quantidadeTotal}`;
+  listaDeCompra.adicionarProdutoNaLista(
+    codigo,
+    imagem,
+    descricao,
+    quantidade,
+    valorTotalProduto
+  );
 };
 
 export default BotaoComprar;
