c42b66b38a8eddc1d120d2aa44f7603e8c172c1c
Hugo Ferreira <hugoogle@terra.com.br>
2021-11-21 22:48:38 -0300

    Implementado rotina para nao permitir caracter invalido

--- componentes/criarCard.js
+++ componentes/criarCard.js
@@ -3,15 +3,59 @@ const CriarCard = (imagem, descricao, preco, indice) => {
   const listaProdutos = document.querySelector("[data-lista-produtos]");
   const produto = document.createElement("div");
   produto.classList.add("card");
-  produto.innerHTML = `
-       <div class="card-body">
-            <img src=${imagem}  class="card-img-top" alt="${descricao}" data-produto-imagem>
-            <p data-produto-descricao>${descricao}</p>
-            <p class="card-price" data-produto-preco> R$ ${preco}</p>
-            <input type="number"  min="0" max="999" class="card-quantidade" value="1" data-produto-quantidade>  
-       </div>`;
+ 
+  const cardBody = document.createElement("div");
+  cardBody.setAttribute("class", "card-body");
+ 
+  const img = document.createElement("img");
+   img.setAttribute("src", imagem);
+  img.setAttribute("class", "card-img-top");
+  img.setAttribute("alt", descricao);
+  img.setAttribute("data-produto-imagem", "");
+  cardBody.appendChild(img);
+ 
+  const pDescricao = document.createElement("p");
+  pDescricao.setAttribute("data-produto-descricao", "");
+  pDescricao.textContent = descricao;
+  cardBody.appendChild(pDescricao);
+ 
+  const pCardPrice = document.createElement("p");
+  pCardPrice.setAttribute("class", "card-price");
+  pCardPrice.setAttribute("data-produto-preco", "");
+  pCardPrice.textContent = `R$ ${preco}`;
+  cardBody.appendChild(pCardPrice);
+
+  const inputQuantidade = document.createElement("input");
+  inputQuantidade.setAttribute("type", "number");
+  inputQuantidade.setAttribute("class", "card-quantidade");
+  inputQuantidade.setAttribute("value", "1");
+  inputQuantidade.setAttribute("data-card", indice);
+  inputQuantidade.setAttribute("data-produto-quantidade", indice);
+  inputQuantidade.oninput = validaQuantidade;
+  inputQuantidade.addEventListener("keypress", somenteNumeros);
+  cardBody.appendChild(inputQuantidade);
+  
+  produto.appendChild(cardBody);
   produto.appendChild(BotaoComprar(indice));
   listaProdutos.appendChild(produto);
+};
+
+const validaQuantidade = (e) => {
+  const input = e.target;
+  if (input.value === "" || input.value < 0) {
+    input.value = 1;
+  }
+};
+
+const somenteNumeros = (e) => {
+  const input = e.target;
+  if (e.key === "," || e.key === "." || e.key === "+" || e.key === "-") {
+    e.preventDefault();
+  }
+
+  if (input.value.length > 3) {
+    input.value = input.value.slice(0, 3);
+  }
 };
 
 export default CriarCard;
